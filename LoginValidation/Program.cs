fc4272945f4f5b53a118e5f6067694559a1f1517
Opilane TTHK <opilane@tthk.ee>
2021-09-29 09:48:38 +0300

    commit

---
+++ LoginValidation/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace LoginValidation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //programm küsib kasutaja sisestada kasutajatunnus;
+            //programm küsib kasutaja sisestada parool;
+            //programm kontrollib, kas mõlemad, nii kasutajatunnus;
+            //kui ka parool on õiged;
+            //kui mõlemad, kasutajatunnus ja parool on õiged;
+            //programm kuvab konsoolis "tere tulemast!";
+            //kui kasutajatunnus või parool on vale, siis
+            //programm kuvab "sisselogimine ebaõnnestus, proovi uuesti"
+            //kt: admin, pr:admin1234
+            //kasutajal on 3 katset
+
+            int i = 0;
+            while (i < 3)
+            {
+                Console.WriteLine("Sisesta kasutajatunnus:");
+                string userName = Console.ReadLine();
+            }
+            
+
+            
+
+        }
+    }
+}
---
+++ WhileLoop/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace WhileLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //programm küsib kasutaja sisestada PIN-koodi
+            //programm võrdleb sisutatud PIN-koodi arvuga 1234
+            //kui sisestatud PIN-kood on 1234
+            //programm kuvab konsoolis "Tere tulemast!"
+            //kui sisestatud PIN on vale, programm kuvab konsoolis
+            //"Vale PIN. Proovi uuesti"
+            //*katse arv on piiramatu
+
+            //boolean --> true/false
+            bool loopActive = true;
+
+            while (loopActive)
+            {
+                Console.WriteLine("Sisesta PIN:");
+                int userPIN = Convert.ToInt32(Console.ReadLine());
+
+                if(userPIN == 1234)
+                {
+                    Console.WriteLine("Tere tulemast!");
+                    loopActive = false;
+                }
+                else
+                {
+                    Console.WriteLine("vale PIN! Proovi uuesti!");
+                }
+            }
+
+            Console.WriteLine("Kena päeva!");
+
+        }
+    }
+}
